b9c01c0dbd502d34fb043e4ffea27647fcaef8ad
gaunt <hao2432@gmail.com>
2024-05-24 18:59:02 +0800

    修改服务端， 根据客户端上传的header来判断是否执行操作

--- server/src/main/java/demo/grpc/server/interceptor/HeaderServerInterceptor.java
+++ server/src/main/java/demo/grpc/server/interceptor/HeaderServerInterceptor.java
@@ -16,13 +16,25 @@ public class HeaderServerInterceptor implements ServerInterceptor {
                                                                  ServerCallHandler<ReqT, RespT> next) {
         logger.info("header received from client:" + requestHeaders);
 
-        return next.startCall(new ForwardingServerCall.SimpleForwardingServerCall<ReqT, RespT>(serverCall) {
-            @Override
-            public void sendHeaders(Metadata responseHeaders) {
-                // 发送给客户端的header
-                // responseHeaders.put(CUSTOM_HEADER_KEY, "customRespondValue");
-                super.sendHeaders(responseHeaders);
-            }
-        }, requestHeaders);
+        // 获取客户端发送的请求头中的特定键值
+        String headerValue = requestHeaders.get(Metadata.Key.of("client_header_key", Metadata.ASCII_STRING_MARSHALLER));
+
+        // 当前客户端上传的client_header_key header值不是123456时就返回请求拒绝
+        if (headerValue != null && headerValue.equals("123456")) {
+            // 如果键值匹配，正常处理请求
+            return next.startCall(new ForwardingServerCall.SimpleForwardingServerCall<ReqT, RespT>(serverCall) {
+                @Override
+                public void sendHeaders(Metadata responseHeaders) {
+                    // 发送给客户端的header
+                    responseHeaders.put(CUSTOM_HEADER_KEY, headerValue);
+                    super.sendHeaders(responseHeaders);
+                }
+            }, requestHeaders);
+        } else {
+            // 如果键值不匹配，可以拒绝请求或进行其他处理
+            serverCall.close(Status.PERMISSION_DENIED.withDescription("Invalid header value for key: client_header_key"), requestHeaders);
+            return new ServerCall.Listener<ReqT>() {}; // 返回一个空监听器，因为请求已被关闭
+        }
+
     }
 }
